ff65b4232080cc84b88606f92c976e4a1b409057
D4rrker <holadarkerktal@gmail.com>
2025-01-22 00:55:24 +0100

    cambio en nombre de parametro

---
+++ src/app/palette/[palette]/page.tsx
@@ -0,0 +1,9 @@
+import GetPathName from "@/app/palets/[palets]/GetPathName";
+
+export default function page() {
+  return (
+    <div className="bg-gray-100 p-6 lg:p-0">
+      <GetPathName />
+    </div>
+  );
+}
